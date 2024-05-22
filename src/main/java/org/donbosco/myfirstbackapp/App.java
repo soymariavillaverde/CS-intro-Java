9afa878728d2692bd8cbd0aad43951b09bcb720d
soymariavillaverde <soymariavillaverde@gmail.com>
2024-05-22 13:11:21 +0200

    Atributes & constructors

--- src/main/java/org/donbosco/myfirstbackapp/App.java
+++ src/main/java/org/donbosco/myfirstbackapp/App.java
@@ -7,11 +7,17 @@ public final class App {
     private App() {
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
+    // Método main base
+    /* public static void main(String[] args) {
+        System.out.println("Hello Wooorld!");
+    } */
+
+    //Prueba de constructores
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        PrimitiveData data = new PrimitiveData();
+        PrimitiveData data2 = new PrimitiveData("Pepe");
+        System.out.println(data.name);
+        System.out.println(data2.name);
     }
+
 }
---
+++ src/main/java/org/donbosco/myfirstbackapp/PrimitiveData.java
@@ -0,0 +1,30 @@
+package org.donbosco.myfirstbackapp;
+
+// imports
+
+public class PrimitiveData {
+    
+    // atributos: valores/propiedades/características de una clase
+    String name;
+
+    
+    
+    // Constructores
+    //Constructor que NO recibe parámetros
+    public PrimitiveData() {
+        // asigna un valor fijo al atributo name
+        this.name = "Porter";
+    }
+
+    //Constructor que recibe parámetros
+    public PrimitiveData(String name) {
+        // asigna un valor variable al atributo name
+        this.name = name;
+    }
+    
+
+
+    //métodos
+
+
+}
